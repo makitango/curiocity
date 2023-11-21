0508c5ae0fddbff49ea676098514130da3e8857c
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-21 09:28:24 +0100

    Add remaining service entries for user database commands

--- backend/src/main/java/com/github/makitango/curiocity/user/UserService.java
+++ backend/src/main/java/com/github/makitango/curiocity/user/UserService.java
@@ -12,4 +12,20 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
 }
