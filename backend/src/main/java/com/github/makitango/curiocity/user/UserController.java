bd2f6c6541b707126777d8fafff839a13b0826a7
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-21 09:27:03 +0100

    Add remaining controller entries for user database commands

--- backend/src/main/java/com/github/makitango/curiocity/user/UserController.java
+++ backend/src/main/java/com/github/makitango/curiocity/user/UserController.java
@@ -17,5 +17,28 @@ public class UserController {
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteUserById(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping(path = {"{id}"})
+    User updateUserID(@PathVariable String id, @RequestBody User user) {
+        if (!user.id().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The id in the url does not match any user's id");
+        }
+        return userService.updateUser(user);
+    }
 }
 
