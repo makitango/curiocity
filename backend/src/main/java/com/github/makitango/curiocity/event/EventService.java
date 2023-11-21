5aed3efcff90316499da3ece0ec2ca479a164c7e
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-21 17:24:38 +0100

    Add post to event service

--- backend/src/main/java/com/github/makitango/curiocity/event/EventService.java
+++ backend/src/main/java/com/github/makitango/curiocity/event/EventService.java
@@ -1,6 +1,7 @@
 package com.github.makitango.curiocity.event;
 
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
 
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
