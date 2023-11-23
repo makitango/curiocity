b8f3031feff1bdfe593f0a3be88dc407893d8f00
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-23 15:37:29 +0100

    Excommunicated vite.svg off the premises

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Add from "./pages/Add";
 import MainPage from "./pages/MainPage";
 
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<MainPage/>}>
-                <Route path="/add" element={<Add/>}/>
-            </Route>
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/add" element={<Add/>}/>
         </Routes>
     );
 }
