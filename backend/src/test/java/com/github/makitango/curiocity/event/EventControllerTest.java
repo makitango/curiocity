5877876af7f301c04491d55c983ac8cc2276e287
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-11-29 02:18:12 +0100

    Add tests for addEvent to check off SonarCloud's test coverage goals

--- backend/src/test/java/com/github/makitango/curiocity/event/EventControllerTest.java
+++ backend/src/test/java/com/github/makitango/curiocity/event/EventControllerTest.java
@@ -16,6 +16,13 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.http.MediaType;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class EventControllerTest {
@@ -28,35 +35,76 @@ class EventControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+// EventControllerTest.java
+
     @Test
     @DirtiesContext
     void getAllEvents_whenEventsExist_thenListOfEventsIsReturnedSuccessfully() throws Exception {
+        // GIVEN
         Event event1 = Event.builder().id("1")
-                .name("Sample Event 1")
-                .location("City Center")
-                .date("1637550000")
-                .time("1637550000")
-                .link("https://example.com/event1")
-                .usersWhoUpvoted(List.of("user1", "user2", "user3"))
-                .usersWhoDownvoted(List.of("user4"))
+                .name("Red Wedding")
+                .location("Westeros")
+                .date("BC1000")
+                .time("5:00 PM")
+                .link("https://example.com/redwedding")
+                .usersWhoUpvoted(List.of("Robb Stark", "Catelyn Stark"))
+                .usersWhoDownvoted(List.of("Walder Frey"))
                 .build();
+
         Event event2 = Event.builder().id("2")
-                .name("Sample Event 2")
-                .location("Park Plaza")
-                .date("1637650000")
-                .time("1637650000")
-                .link("https://example.com/event2")
-                .usersWhoUpvoted(List.of("user1", "user3", "user5"))
-                .usersWhoDownvoted(List.of("user2"))
+                .name("Battle of the Bastards")
+                .location("Winterfell")
+                .date("AC300")
+                .time("2:30 PM")
+                .link("https://example.com/battleofthebastards")
+                .usersWhoUpvoted(List.of("Jon Snow", "Sansa Stark"))
+                .usersWhoDownvoted(List.of("Ramsay Bolton"))
                 .build();
+
         eventRepository.save(event1);
         eventRepository.save(event2);
 
+        // WHEN
         List<Event> expected = List.of(event1, event2);
         String expectedAsJson = objectMapper.writeValueAsString(expected);
 
+        // THEN
         mockMvc.perform(get(BASE_URI))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedAsJson));
+    }
+
+    @Test
+    @DirtiesContext
+    void addEvent_whenEventIsValid_thenEventIsAddedSuccessfully() throws Exception {
+        // GIVEN
+        EventDTO newEventDTO = EventDTO.builder()
+                .name("Battle of Blackwater")
+                .location("King's Landing")
+                .date("AC299")
+                .time("8:00 PM")
+                .link("https://example.com/battleofblackwater")
+                .usersWhoUpvoted(List.of("Tyrion Lannister", "Davos Seaworth"))
+                .usersWhoDownvoted(List.of("Stannis Baratheon"))
+                .build();
+
+        // WHEN
+        MvcResult result = mockMvc.perform(post(BASE_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newEventDTO)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Event addedEvent = objectMapper.readValue(result.getResponse().getContentAsString(), Event.class);
+
+        // THEN
+        assertNotNull(addedEvent);
+        assertEquals("King's Landing", addedEvent.location());
+        assertEquals("Battle of Blackwater", addedEvent.name());
+        assertEquals("AC299", addedEvent.date());
+        assertEquals("8:00 PM", addedEvent.time());
+        assertEquals("https://example.com/battleofblackwater", addedEvent.link());
+        assertEquals(List.of("Tyrion Lannister", "Davos Seaworth"), addedEvent.usersWhoUpvoted());
+        assertEquals(List.of("Stannis Baratheon"), addedEvent.usersWhoDownvoted());
     }
 }
--- backend/src/test/java/com/github/makitango/curiocity/event/EventServiceTest.java
+++ backend/src/test/java/com/github/makitango/curiocity/event/EventServiceTest.java
@@ -3,6 +3,7 @@ package com.github.makitango.curiocity.event;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class EventServiceTest {
@@ -15,30 +16,74 @@ class EventServiceTest {
         // GIVEN
         Event event1 = Event.builder()
                 .id("1")
-                .name("Sample Event 1")
-                .location("City Center")
-                .date("1637550000")
-                .time("1637550000")
-                .link("https://example.com/event1")
-                .usersWhoUpvoted(List.of("user1", "user2", "user3"))
-                .usersWhoDownvoted(List.of("user4"))
+                .name("Battle of Winterfell")
+                .location("Winterfell")
+                .date("AC302")
+                .time("9:00 PM")
+                .link("https://example.com/battleofwinterfell")
+                .usersWhoUpvoted(List.of("Arya Stark", "Jon Snow", "Daenerys Targaryen"))
+                .usersWhoDownvoted(List.of("Night King"))
                 .build();
+
         Event event2 = Event.builder()
                 .id("2")
-                .name("Sample Event 2")
-                .location("Park Plaza")
-                .date("1637650000")
-                .time("1637650000")
-                .link("https://example.com/event2")
-                .usersWhoUpvoted(List.of("user1", "user3", "user5"))
-                .usersWhoDownvoted(List.of("user2"))
+                .name("The Purple Wedding")
+                .location("King's Landing")
+                .date("AC300")
+                .time("3:00 PM")
+                .link("https://example.com/purplewedding")
+                .usersWhoUpvoted(List.of("Sansa Stark", "Margaery Tyrell"))
+                .usersWhoDownvoted(List.of("Joffrey Baratheon"))
                 .build();
+
         List<Event> expected = List.of(event1, event2);
+
         // WHEN
         when(mockEventRepository.findAll()).thenReturn(expected);
         List<Event> actual = eventService.getAllEvents();
+
         // THEN
         verify(mockEventRepository).findAll();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void addEvent_shouldReturnAddedEvent() {
+        // GIVEN
+        EventDTO eventDTO = EventDTO.builder()
+                .name("Battle of the Bastards")
+                .location("Winterfell")
+                .date("AC300")
+                .time("2:30 PM")
+                .link("https://example.com/battleofthebastards")
+                .usersWhoUpvoted(List.of("Jon Snow", "Sansa Stark"))
+                .usersWhoDownvoted(List.of("Ramsay Bolton"))
+                .build();
+
+        Event expectedEvent = Event.builder()
+                .id("3")
+                .name("Battle of the Bastards")
+                .location("Winterfell")
+                .date("AC300")
+                .time("2:30 PM")
+                .link("https://example.com/battleofthebastards")
+                .usersWhoUpvoted(List.of("Jon Snow", "Sansa Stark"))
+                .usersWhoDownvoted(List.of("Ramsay Bolton"))
+                .build();
+
+        // WHEN
+        when(mockEventRepository.save(any(Event.class))).thenReturn(expectedEvent);
+        Event addedEvent = eventService.addEvent(eventDTO);
+
+        // THEN
+        verify(mockEventRepository).save(any(Event.class));
+        assertNotNull(addedEvent);
+        assertEquals("Battle of the Bastards", addedEvent.name());
+        assertEquals("Winterfell", addedEvent.location());
+        assertEquals("AC300", addedEvent.date());
+        assertEquals("2:30 PM", addedEvent.time());
+        assertEquals("https://example.com/battleofthebastards", addedEvent.link());
+        assertEquals(List.of("Jon Snow", "Sansa Stark"), addedEvent.usersWhoUpvoted());
+        assertEquals(List.of("Ramsay Bolton"), addedEvent.usersWhoDownvoted());
     }
 }
