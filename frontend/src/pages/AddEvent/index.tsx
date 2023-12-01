731b4e1cf2079b2af73db1a38d833fde13d1def2
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-12-01 18:41:52 +0100

    Consolidate form code lines

--- frontend/src/pages/AddEvent/index.tsx
+++ frontend/src/pages/AddEvent/index.tsx
@@ -1,40 +1,40 @@
-import {useState, ChangeEvent, FormEvent, useEffect} from "react";
-import {Link} from "react-router-dom";
-import axios from "axios";
-import {AddEventType} from "../../resources/types.tsx";
+import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import axios from 'axios';
+import { AddEventType } from '../../resources/types.tsx';
+import { Link } from 'react-router-dom';
 
 export default function AddEvent(): JSX.Element {
     const initialFormData: AddEventType = {
-        name: "",
-        location: "",
-        date: "",
-        time: "",
-        link: "",
+        name: '',
+        location: '',
+        date: '',
+        time: '',
+        link: '',
     };
 
     const [formData, setFormData] = useState<AddEventType>(initialFormData);
     const [eventCreated, setEventCreated] = useState<boolean>(false);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-        const {name, value} = e.target;
-        setFormData({
-            ...formData,
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
             [name]: value,
-        });
+        }));
     };
 
     const handleSubmit = (e: FormEvent): void => {
         e.preventDefault();
 
         axios
-            .post("/api/events", formData)
-            .then((response): void => {
-                console.log("Event added successfully:", response.data);
+            .post('/api/events', formData)
+            .then((response) => {
+                console.log('Event added successfully:', response.data);
                 setEventCreated(true);
                 setFormData(initialFormData);
             })
-            .catch((error): void => {
-                console.error("Error adding event:", error);
+            .catch((error) => {
+                console.error('Error adding event:', error);
             });
     };
 
@@ -42,7 +42,7 @@ export default function AddEvent(): JSX.Element {
         let timer: ReturnType<typeof setTimeout>;
 
         if (eventCreated) {
-            timer = setTimeout((): void => {
+            timer = setTimeout(() => {
                 setEventCreated(false);
             }, 5000);
         }
@@ -50,71 +50,32 @@ export default function AddEvent(): JSX.Element {
         return () => clearTimeout(timer);
     }, [eventCreated]);
 
-
     return (
         <>
             <Link to="/">MainPage</Link>
             <h1>Add Event</h1>
             <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Name:</span>
-                    <input
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <br/>
-                <label>
-                    <span>Location:</span>
-                    <input
-                        type="text"
-                        name="location"
-                        value={formData.location}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <br/>
-                <label>
-                    <span>Date:</span>
-                    <input
-                        type="text"
-                        name="date"
-                        value={formData.date}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <br/>
-                <label>
-                    <span>Time:</span>
-                    <input
-                        type="text"
-                        name="time"
-                        value={formData.time}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <br/>
-                <label>
-                    <span>Link:</span>
-                    <input
-                        type="text"
-                        name="link"
-                        value={formData.link}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <br/>
+                {Object.entries(initialFormData).map(([key]) => {
+                    const inputKey = key as keyof AddEventType; // Explicitly cast key to keyof AddEventType
+                    return (
+                        <div key={key}>
+                            <label>
+                                <span>{key.charAt(0).toUpperCase() + key.slice(1)}:</span>
+                                <input
+                                    type="text"
+                                    name={key}
+                                    value={formData[inputKey]}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                            <br />
+                        </div>
+                    );
+                })}
                 {eventCreated && <p>Event created</p>}
                 <button type="submit">Add Event</button>
             </form>
-
         </>
     );
 }
