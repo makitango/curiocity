5521ba74a80cea6d82af3b2415542423267e74c7
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-12-09 19:47:53 +0100

    Update backend/src/main/java/com/github/makitango/curiocity/event/exceptions/EventNotFoundException.java
    Add direct message embed
    
    Co-authored-by: Aljoscha Nyang <70570811+AljoschaNy@users.noreply.github.com>

--- backend/src/main/java/com/github/makitango/curiocity/event/exceptions/EventNotFoundException.java
+++ backend/src/main/java/com/github/makitango/curiocity/event/exceptions/EventNotFoundException.java
@@ -1,14 +1,11 @@
 package com.github.makitango.curiocity.event.exceptions;
 
 public class EventNotFoundException extends RuntimeException {
-
-    public static final String DEFAULT_MESSAGE = "Event not found";
-
     public EventNotFoundException(String message) {
         super(message);
     }
 
     public EventNotFoundException() {
-        super(DEFAULT_MESSAGE);
+        super("Event not found");
     }
 }
